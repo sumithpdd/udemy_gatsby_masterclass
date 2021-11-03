145b850b421c6d8ddd3941c9bb5c55ecfe622a1d
Sumith Damodaran <sumithpd@gmail.com>
2021-11-03 11:26:06 +0000

    Gatsby source filesystem plugin

--- gatsby-garb/src/pages/posts/page-3.js
+++ gatsby-garb/src/pages/posts/page-3.js
@@ -1,18 +1,59 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link, StaticQuery } from "gatsby"
 
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
+const getImageData = graphql`
+  {
+    allFile {
+      edges {
+        node {
+          relativePath
+          size
+          extension
+          birthTime
+        }
+      }
+    }
+  }
+`
 
-const SecondPage = () => (
+const thirdPage = () => (
   <Layout>
     <Seo title="Page three" />
     <h1>Hi from the page3 </h1>
     <p>Welcome to page 3</p>
+    <h3>Image File Data</h3>
+    <StaticQuery
+      query={getImageData}
+      render={data => (
+        <table>
+          <thead>
+            <tr>
+              <th>Relative Path</th>
+              <th>size of image</th>
+              <th>Extension</th>
+              <th>Birthtime of Image</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({node}, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.size}</td>
+                <td>{node.extension}</td>
+                <td>{node.birthTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    />
+
     <Link to="/page-2">Go to page 2</Link>
-    <br/>
+    <br />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default thirdPage
