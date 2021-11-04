a9b04480ba64d12c674faa1ff57b3a6d91c07ef3
Sumith Damodaran <sumithpd@gmail.com>
2021-11-04 15:24:43 +0000

    Adding Pagation and configuration for multiple markdown posts

--- gatsby-garb/gatsby-node.js
+++ gatsby-garb/gatsby-node.js
@@ -2,6 +2,7 @@ const path = require("path")
 const { createFilePath } = require("gatsby-source-filesystem")
 
 const PostTemplate = path.resolve("./src/templates/post-template.js")
+const BlogTemplate = path.resolve("./src/templates/blog-template.js")
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -21,12 +22,35 @@ exports.createPages = async ({ graphql, actions }) => {
   `)
 
   const posts = result.data.allMarkdownRemark.edges
+  
   posts.forEach(({ node: post }) => {
     createPage({
       path: `posts${post.fields.slug}`,
       component: PostTemplate,
       context: {
         slug: post.fields.slug,
+      },
+    })
+  })
+  
+  const postsPerPage = 2
+  const totalPages = Math.ceil(posts.length / postsPerPage)
+
+  Array.from({length:totalPages}).forEach((_, index) => {
+    const currentPage = index + 1
+    const isFirstPage = index === 0
+    const isLastPage = currentPage === totalPages
+
+    createPage({
+      path: isFirstPage ? "/blog" : `/blog/${currentPage}`,
+      component: BlogTemplate,
+      context: {
+        limit: postsPerPage,
+        skip: index * postsPerPage,
+        isFirstPage,
+        isLastPage,
+        currentPage,
+        totalPages
       },
     })
   })
